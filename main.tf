6802ff5be690c4677313713bc145a465fb8988f1
ashishdv456 <ashishdv456@gmail.com>
2026-02-14 19:30:08 +0530

    Modify resource group and remove unused resources
    Updated resource group to 'ashish-centralindia-rg' in Central India and removed storage account and virtual network resources.

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-resource "azurerm_storage_account" "Ashish122" {
-  name                     = "ashish1a22"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-}
-resource "azurerm_virtual_network" "vnet1283" {
-  name                = "Ashish2-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_resource_group" "rg_centralindia" {
+  name     = "ashish-centralindia-rg"
+  location = "Central India"
 }
